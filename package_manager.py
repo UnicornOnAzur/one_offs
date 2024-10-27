ad9c835f31bdc1b2ebafcae4af166381f7a734ad
QtyPython2020 <htm131016@gmail.com>
2024-10-27 23:08:43 +0100

    Refactored exception handling and formatting

--- package_manager.py
+++ package_manager.py
@@ -2,13 +2,20 @@
 """
 @author: UnicornOnAzur
 
-
+This script imports modules dynamically, installing them if they are not
+already present. It utilizes the importlib and subprocess modules to achieve
+this functionality, while also handling potential errors.
 """
 import importlib
 import subprocess
 import sys
 import typing
 import warnings
+#
+SWAP: dict = {"PIL": "Pillow",
+              "sklearn": "scikit-learn",
+              "flask_sqlalchemy": "Flask-SQLAlchemy",
+              "dateutil": "python-dateutil"}
 
 
 def _pip_install_package(name: str) -> bool:
@@ -27,21 +34,20 @@ def _pip_install_package(name: str) -> bool:
 
     """
     try:
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', name])
+        subprocess.check_call([sys.executable, "-m", "pip", "install", name])
         return True
     except subprocess.CalledProcessError as e:
         warnings.warn(f"Failed to install package '{name}': {e}", UserWarning)
         return False
     except FileNotFoundError:
         warnings.warn(
-            "Pip might not be installed. Please install pip to use this function.",
+        "Pip might not be installed. Please install pip to use this function.",
             UserWarning)
         return False
 
 
 def import_module(name: str,
                   package: str = None,
-                  retries: int = 2
                   ) -> typing.Optional[typing.types.ModuleType]:
     """
     Import a module, installing it if not found.
@@ -53,8 +59,6 @@ def import_module(name: str,
     package : str, optional
         The package name if the module is part of a package. The default is
         None.
-    retries : int, optional
-        The number of retries for importing the module. The default is 2.
 
     Returns
     -------
@@ -62,25 +66,17 @@ def import_module(name: str,
         The imported module or None if the import fails.
 
     """
-    if retries <= 0:
-        warnings.warn(
-            "Maximum retries reached without success for module import.",
-            UserWarning)
-        return None
     try:
         return importlib.import_module(name, package=package)
     except ModuleNotFoundError:
         print(f"Module '{name}' not found. Attempting to install...")
         if _pip_install_package(name):  # Check if installation was successful
-            return import_module(name, package=package, retries=retries - 1)
+            return import_module(name, package=package)
         else:
             warnings.warn(
                 f"Installation of module '{name}' failed. Cannot import.",
                 UserWarning)
             return None
-    except (TypeError, ValueError, ImportError) as e:
-        warnings.warn(f"Error importing module '{name}': {e}", UserWarning)
-        return None
 
 
 def demo():
