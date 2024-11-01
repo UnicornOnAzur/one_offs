1f830fc0c735c1088e810bfac406945f259a3ffb
QtyPython2020 <htm131016@gmail.com>
2024-11-01 18:44:55 +0100

    Refactored functions

--- package_manager.py
+++ package_manager.py
@@ -2,29 +2,71 @@
 """
 @author: UnicornOnAzur
 
-This script imports modules dynamically, installing them if they are not
+This script dynamically imports modules, installing them if they are not
 already present. It utilizes the importlib and subprocess modules to achieve
-this functionality, while also handling potential errors.
+this functionality while handling potential errors.
 """
+# Standard library
 import importlib
 import subprocess
 import sys
 import typing
 import warnings
-#
-SWAP: dict = {"PIL": "Pillow",
-              "sklearn": "scikit-learn",
-              "flask_sqlalchemy": "Flask-SQLAlchemy",
-              "dateutil": "python-dateutil"}
+# Mapping of common package names to their installable counterparts. The key is
+# the import name and the value is the install name.
+PACKAGE_MAP: dict = {"bs4": "beautifulsoup4",
+                     "dateutil": "python-dateutil",
+                     "flask_sqlalchemy": "Flask-SQLAlchemy",
+                     "PIL": "Pillow",
+                     "sklearn": "scikit-learn",
+                     "werkzeug": "Flask"
+                     }
 
 
-def _pip_install_package(name: str) -> bool:
+def _get_package_name(module_name: str, package: str = None) -> str:
+    """
+
+
+    Parameters
+    ----------
+    module_name : str
+        The name of the module.
+    package : str, optional
+        The package name if the module is part of a package. The default is
+        None.
+
+    Raises
+    ------
+    TypeError
+        If the module name starts with a dot and no package is provided.
+
+    Returns
+    -------
+    str
+        The resolved package name.
+
+    """
+    # if it is a relative import check if package is supplied
+    if module_name.startswith("."):
+        # raise an error if no package is supplied
+        if not package:
+            raise TypeError(
+                "Package must be provided when name starts with a dot.")
+        package_name: str = package.split(".")[0]
+    else:
+        package_name: str = module_name.split(".")[0].strip()
+        if not package_name:
+            raise TypeError("Module name cannot be resolved.")
+    return PACKAGE_MAP.get(package_name, package_name)
+
+
+def _pip_install_package(package: str) -> bool:
     """
     Install a package using pip.
 
     Parameters
     ----------
-    name : str
+    package : str
         The name of the package to install.
 
     Returns
@@ -34,20 +76,21 @@ def _pip_install_package(name: str) -> bool:
 
     """
     try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", name])
+        subprocess.check_call([sys.executable,
+                               "-m", "pip", "install",
+                               package])
         return True
     except subprocess.CalledProcessError as e:
-        warnings.warn(f"Failed to install package '{name}': {e}", UserWarning)
+        warnings.warn(f"Failed to install package '{package}': {e}",
+                      UserWarning)
         return False
     except FileNotFoundError:
-        warnings.warn(
-        "Pip might not be installed. Please install pip to use this function.",
-            UserWarning)
+        warnings.warn("Pip might not be installed. Please install pip to use this function.",  # noqa E501
+                      UserWarning)
         return False
 
 
-def import_module(name: str,
-                  package: str = None,
+def import_module(name: str, package: str = None,
                   ) -> typing.Optional[typing.types.ModuleType]:
     """
     Import a module, installing it if not found.
@@ -70,13 +113,15 @@ def import_module(name: str,
         return importlib.import_module(name, package=package)
     except ModuleNotFoundError:
         print(f"Module '{name}' not found. Attempting to install...")
-        if _pip_install_package(name):  # Check if installation was successful
+        package_name: str = _get_package_name(name, package=package)
+        # Check if installation was successful
+        if _pip_install_package(package_name):
             return import_module(name, package=package)
-        else:
-            warnings.warn(
-                f"Installation of module '{name}' failed. Cannot import.",
-                UserWarning)
-            return None
+
+        warnings.warn(
+            f"Installation of module '{name}' failed. Cannot import.",
+            UserWarning)
+        return None
 
 
 def demo():
