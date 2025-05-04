a158b052e739b8f125c667b401b635f39fb7e068
UnicornOnAzur <unicorn.on.azur@gmail.com>
2025-05-04 20:08:46 +0200

    Refactored make_map function

--- gpx_map.py
+++ gpx_map.py
@@ -27,87 +27,29 @@ def make_map(
     Generates a folium map with GPX tracks.
 
     This function reads GPX files, extracts their coordinates, and creates a
-    map with polylines representing the tracks.
+    map with polylines representing the tracks that is fitted to .
 
     Parameters:
-        file_list : A single GPX file or a list of GPX files to be plotted on
-        the map.
+        file_list : An optional list of GPX files to be plotted on the map.
 
     Returns:
         The rendered HTML representation of the folium map.
     """
-    def _read_gpx(file_name):
-        """Reads a GPX file and returns its contents as a GeoDataFrame.
-
-        Parameters:
-            file_name : The GPX file to read.
-
-        Returns:
-            The GeoDataFrame containing the GPX data.
-        """
-        return gpd.read_file(file_name, layer="tracks")
-
-    def get_coordinates(
-        file: str
-            ) -> gpd.GeoDataFrame:
-        """
-        Extracts coordinates from a GPX file and reverses the order of 'x' and
-        'y'.
-
-        Parameters:
-            file : The GPX file from which to extract coordinates.
-
-        Returns:
-            A DataFrame containing the coordinates with 'y' and 'x' columns.
-        """
-        return _read_gpx(file).geometry.get_coordinates()[["y", "x"]]
-
-    def make_polyline(
-        gpx_file: UPLOADED_FILE,
-        color: str
-            ) -> folium.FeatureGroup:
-        """
-        Creates a polyline feature for a GPX file.
-
-        Parameters:
-            gpx_file : The GPX file to create a polyline for.
-            color : The color of the polyline.
-
-        Returns:
-            The feature group containing the polyline.
-        """
-        lines_layer: folium.FeatureGroup = folium.FeatureGroup(name=gpx_file)
-        coords: gpd.GeoDataFrame = get_coordinates(gpx_file)
-        folium.PolyLine(locations=coords,
-                        color=color).add_to(lines_layer)
-        return lines_layer
-
-    def update_bounds(
-        file_list: UPLOADED_FILE
-            ) -> tuple[float]:
-        """
-        Updates the bounds of the map based on the GPX files.
-
-        Parameters:
-            file_list : A list of GPX files to calculate bounds from.
-
-        Returns:
-            A tuple containing the minimum and maximum bounds.
-        """
-        gdf: gpd.GeoDataFrame = pd.concat(map(_read_gpx, file_list))
-        bounds: tuple[float] = gdf.geometry.total_bounds
-        return bounds
-
     colormap: typing.List[str] = list(map(mpl.colors.rgb2hex,
                                           mpl.colormaps["Set1"].colors))
     map_: folium.Map = folium.Map()
+    gdfs: typing.List[gpd.GeoDataFrame] = []
     for file_name, color in zip(file_list or [], colormap):
-        make_polyline(file_name, color).add_to(map_)
-
+        gdf: gpd.GeoDataFrame = gpd.read_file(file_name, layer="tracks")
+        gdfs.append(gdf)
+        lines_layer: folium.FeatureGroup = folium.FeatureGroup(name=file_name)
+        folium.PolyLine(locations=gdf.geometry.get_coordinates()[["y", "x"]],
+                        color=color).add_to(lines_layer)
+        lines_layer.add_to(map_)
     # Fit the extent of the map to the tracks
-    if file_list:
-        xmin, ymin, xmax, ymax = update_bounds(file_list)
-        map_.fit_bounds([[ymin, xmin], [ymax, xmax]])
+    xmin, ymin, xmax, ymax = pd.concat(gdfs).total_bounds if gdfs\
+        else [-50, -50, 50, 50]
+    map_.fit_bounds([[ymin, xmin], [ymax, xmax]])
     return folium.Figure().add_child(map_).render()
 
 
