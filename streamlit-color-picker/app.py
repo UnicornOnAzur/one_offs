88cc2849235fcbae90059418d10ea5720f04db12
UnicornOnAzur <unicorn.on.azur@gmail.com>
2026-07-15 22:26:19 +0200

    Test runtime context

--- streamlit-color-picker/app.py
+++ streamlit-color-picker/app.py
@@ -8,15 +8,19 @@ and manage user interactions through a Streamlit interface.
 author:
 """
 # Standard library
+import functools
 import glob
 import pathlib
-import toml
+import tomllib
+import types
 import typing
 # Third party
 import pandas as pd
 import streamlit as st
+import toml
 # Constants
-FOLDER_PATH: str = "**/*streamlit-color-picker*/"
+st.write(st.runtime.scriptrunner_utils.script_run_context.get_script_run_ctx().context_info.url)
+FOLDER_PATH: str = ""  # "**/*streamlit-color-picker*/"
 TOML_FILE_PATH: pathlib.Path = glob.glob(f"{FOLDER_PATH}.streamlit/*.toml",
                                          recursive=True)[0]
 TXT_FILE_PATH: pathlib.Path = glob.glob(f"{FOLDER_PATH}default.txt",
@@ -24,21 +28,111 @@ TXT_FILE_PATH: pathlib.Path = glob.glob(f"{FOLDER_PATH}default.txt",
 HEADER: str = "theme"
 
 
-def _download_button(column: st._DeltaGenerator):
-    return column.download_button(
-        "Download button", data=open(TOML_FILE_PATH, "rb"),
-        file_name="config.toml")
+def set_place_of_a_widget_on_a_row_of(number_of_columns: int = 2
+                                      ) -> typing.Callable[...,
+                                                           st._DeltaGenerator]:
+    """
+    A decorator to arrange the widget in a specified number of columns.
+
+    Parameters:
+        number_of_columns : The number of columns to arrange the outputs.
+
+    Returns:
+        A wrapper function that manages column placement.
+    """
+    def decorator(func: typing.Callable[
+        [str, st._DeltaGenerator | types.ModuleType], st._DeltaGenerator]
+              ) -> typing.Callable[..., st._DeltaGenerator]:
+        """
+        Wraps the original function to manage column placement. Initializes
+        wrapper.count and wrapper.columns to maintain state.
+
+        Parameters:
+            func : The function to be wrapped.
+
+        Returns:
+            The wrapper function that manages column placement.
+        """
+        @functools.wraps(func)
+        def wrapper(
+                *args: typing.Tuple[str, st._DeltaGenerator | types.ModuleType]
+                ) -> st._DeltaGenerator:
+            """
+            Manages the placement of widgets in columns. Uses wrapper.count and
+            wrapper.columns to maintain state.
+
+            Parameters:
+                *args : The field name and page segment.
+
+            Returns:
+                st._DeltaGenerator: The widget created by the inner function.
+            """
+            name, page_segment = args
+            if wrapper.count % number_of_columns == 0:
+                wrapper.columns = page_segment.columns(number_of_columns)
+            widget: st._DeltaGenerator = func(
+                name, wrapper.columns[wrapper.count % number_of_columns])
+            wrapper.count += 1
+            return widget
+        wrapper.count = 0
+        wrapper.columns = []  # typing.List[st._DeltaGenerator]
+        return wrapper
+    return decorator
+
+
+@set_place_of_a_widget_on_a_row_of(3)
+def _color_picker(
+        field_name: str, column: st._DeltaGenerator
+        ) -> st._DeltaGenerator:
+    """
+    Creates a color picker widget in the specified column.
+
+    Parameters:
+        field_name : The name of the field for the color picker.
+        column : The column where the color picker will be placed.
+
+    Returns:
+        The color picker widget.
+    """
+    return column.color_picker(
+        field_name, value=st.session_state.get(field_name, None),
+        key=field_name)
+
+
+def _download_button(column: st._DeltaGenerator) -> bool:
+    with open(TOML_FILE_PATH, "rb") as file:
+        return column.download_button("Download button", data=file,
+                                      file_name="config.toml")
+
+
+def _uploader(column: st._DeltaGenerator, name: str) -> None:
+    uploaded_file: typing.Optional[
+        st.runtime.uploaded_file_manager.UploadedFile] = column.file_uploader(
+        "Upload a TOML file.", type=".toml",
+        key=f"uploader_{name}_{st.session_state.uploader_key}")
+    if uploaded_file is not None:
+        with open(TOML_FILE_PATH, "w") as file:
+            toml.dump(tomllib.load(uploaded_file), file)
+        st.session_state.uploader_key += 1
 
 
 def read_toml_to_dict() -> typing.Dict[str, str]:
-    with open(TOML_FILE_PATH) as file:
-        return toml.load(file)[HEADER]
+    """
+    Read the TOML file to a dictionary and read the keys within the 'theme'
+    header.
+    """
+    with open(TOML_FILE_PATH, "rb") as file:
+        return tomllib.load(file)[HEADER]
 
 
 def write_session_state_to_toml() -> None:
+    """
+    Creates a TOML file by creating a dictionary from the session state and add
+    a header.
+    """
     with open(TOML_FILE_PATH, "w") as file:
-        file.write(f"[{HEADER}]\n")
-        toml.dump({key: st.session_state[key] for key in COLOR_FIELDS}, file)
+        toml.dump({HEADER:
+                   {key: st.session_state[key] for key in COLOR_FIELDS}}, file)
 
 
 def reset_toml_file_to_default() -> None:
@@ -47,31 +141,9 @@ def reset_toml_file_to_default() -> None:
         toml_file.write(text_file.read())
 
 
-@lambda _: _()
-def init() -> None:
-    global COLOR_FIELDS
-    COLOR_FIELDS = []
-    st.set_page_config(layout="wide")
-    theme: typing.Dict[str, str] = read_toml_to_dict()
-    for key, value in theme.items():
-        st.session_state[key] = value
-        COLOR_FIELDS.append(key)
-
-
-def set_new_color_in_theme():
-    """Sets a new color in the theme based on user input and updates the
-    TOML configuration file.
-
-    Returns:
-        None
-    """
-    st.session_state[st.session_state["color_field"]] =\
-        st.session_state["color"]
-    write_session_state_to_toml()
-
-
 def color_cells_by_value(values: pd.Series) -> typing.List[str]:
-    """Applies background color styles to cells based on their values.
+    """
+    Applies background color styles to cells based on their values.
 
     Parameters:
         values: A list of color values.
@@ -83,6 +155,20 @@ def color_cells_by_value(values: pd.Series) -> typing.List[str]:
 
 
 def main() -> None:
+    """
+    """
+    if "uploader_key" not in st.session_state:
+        st.session_state["uploader_key"] = 0
+    global COLOR_FIELDS
+    COLOR_FIELDS = []
+    st.set_page_config(layout="wide")
+    theme: typing.Dict[str, str] = read_toml_to_dict()
+    for key, value in theme.items():
+        st.session_state[key] = value
+        COLOR_FIELDS.append(key)
+    for field_name in COLOR_FIELDS:
+        st.session_state[f"{field_name}_1"] = st.session_state[field_name]
+
     sidebar = st.sidebar
     sidebar.header("Sidebar")
     sc1, sc2, sc3 = sidebar.columns(3)
@@ -92,7 +178,7 @@ def main() -> None:
     sc3.button("Tertiary button", type="tertiary")
     sc1.link_button("Link button", url="")
     _download_button(sc2)
-    sc3.file_uploader("Uploader")
+    _uploader(sc3, "sc3")
     sidebar.multiselect("Multiselect", options=COLOR_FIELDS,
                         default=COLOR_FIELDS[-1])
     container_sidebar = sidebar.container(border=True)
@@ -107,11 +193,9 @@ def main() -> None:
     # LEFT: changing colors and displaying the current value
     left.header("Change a color")
     form = left.form("form")
-    form.color_picker("Choose a color",  key="color")
-    form.selectbox("Where do you want to set the color?",
-                   options=COLOR_FIELDS, key="color_field")
-    if form.form_submit_button():
-        set_new_color_in_theme()
+    for field in COLOR_FIELDS:
+        _color_picker(field, form)
+    form.form_submit_button(on_click=write_session_state_to_toml)
     left.subheader("The current colors")
     left.table(pd.DataFrame().from_dict(read_toml_to_dict(), orient="index"
                                         ).style.apply(color_cells_by_value,
@@ -126,7 +210,7 @@ def main() -> None:
     rc3.button("Tertiary button", type="tertiary")
     rc1.link_button("Link button", url="")
     _download_button(rc2)
-    rc3.file_uploader("Uploader")
+    _uploader(rc3, "rc3")
     right.multiselect("Multiselect", options=COLOR_FIELDS,
                       default=COLOR_FIELDS[-1])
     container = right.container(border=True)
